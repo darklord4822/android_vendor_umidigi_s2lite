f7974ac9c0b906bc1e925e7468936580add99746
darklord4822 <darklord4722@gmail.com>
2018-04-21 01:47:35 +0000

    first commit

---
+++ s2lite-vendor.mk
@@ -0,0 +1,21 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/s2lite/vendor/thh,system/vendor/thh)
